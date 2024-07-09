4ca60f0fa9af7d413557fb7f652ebd63c5dace01
Chirag Khaitan <chiragkhaitan2014@gmail.com>
2024-07-10 04:19:33 +0530

    Update route.ts

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -49,9 +49,9 @@ export async function POST(req: Request) {
 
         //notify all connected chat room clients
 
-        pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message)
+        await pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message)
 
-        pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message',
+        await pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message',
             {
                 ...message,
                 senderImg: sender.image,
